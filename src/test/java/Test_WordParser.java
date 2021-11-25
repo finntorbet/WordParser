934822b9674f5e58633a78e95cc89025907320bd
Taerbit <finnt2@gmail.com>
2021-11-25 12:49:19 +0000

    Fixed bible expected string with change to regex

--- src/test/java/Test_WordParser.java
+++ src/test/java/Test_WordParser.java
@@ -39,30 +39,29 @@ public class Test_WordParser {
 
     @Test
     public void test_bible() throws WordParserException {
-        assertEquals("Word count = 792344\n" +
-                            "Average word length = 4.116\n" +
-                            "Number of words of length 1 is 18278\n" +
-                            "Number of words of length 2 is 129963\n" +
-                            "Number of words of length 3 is 219689\n" +
-                            "Number of words of length 4 is 173583\n" +
-                            "Number of words of length 5 is 97116\n" +
-                            "Number of words of length 6 is 53730\n" +
-                            "Number of words of length 7 is 41348\n" +
-                            "Number of words of length 8 is 25690\n" +
-                            "Number of words of length 9 is 17226\n" +
-                            "Number of words of length 10 is 7979\n" +
-                            "Number of words of length 11 is 4048\n" +
-                            "Number of words of length 12 is 1841\n" +
-                            "Number of words of length 13 is 934\n" +
-                            "Number of words of length 14 is 746\n" +
-                            "Number of words of length 15 is 119\n" +
-                            "Number of words of length 16 is 26\n" +
-                            "Number of words of length 17 is 12\n" +
-                            "Number of words of length 18 is 8\n" +
-                            "Number of words of length 19 is 4\n" +
-                            "Number of words of length 20 is 3\n" +
-                            "Number of words of length 27 is 1\n" +
-                            "The most frequently occurring word length is 219689, for word length of 3",
+        assertEquals("Word count = 792384\n" +
+                        "Average word length = 4.111\n" +
+                        "Number of words of length 1 is 18287\n" +
+                        "Number of words of length 2 is 130347\n" +
+                        "Number of words of length 3 is 219826\n" +
+                        "Number of words of length 4 is 174044\n" +
+                        "Number of words of length 5 is 96700\n" +
+                        "Number of words of length 6 is 53590\n" +
+                        "Number of words of length 7 is 41257\n" +
+                        "Number of words of length 8 is 25596\n" +
+                        "Number of words of length 9 is 17175\n" +
+                        "Number of words of length 10 is 7922\n" +
+                        "Number of words of length 11 is 4009\n" +
+                        "Number of words of length 12 is 1823\n" +
+                        "Number of words of length 13 is 916\n" +
+                        "Number of words of length 14 is 735\n" +
+                        "Number of words of length 15 is 115\n" +
+                        "Number of words of length 16 is 20\n" +
+                        "Number of words of length 17 is 9\n" +
+                        "Number of words of length 18 is 7\n" +
+                        "Number of words of length 19 is 3\n" +
+                        "Number of words of length 20 is 3\n" +
+                        "The most frequently occurring word length is 219826, for word length of 3",
             WordParser.parseFile(path + "bible_daily.txt"));
     }
 
